338f37bc97c90ffd6e3c06b1e0c22f23201109db
Amrith369 <mark.siminski@connect.kwoods.org>
2017-11-03 15:20:39 -0400

    Various-Changes
    -Changed Terrain
    -Altered Menu UI
    -Added Functionality To Map And Exit Buttons

--- MMOMOG/Assets/Scripts/PlayerControllerScripts/CharacterControl.cs
+++ MMOMOG/Assets/Scripts/PlayerControllerScripts/CharacterControl.cs
@@ -10,8 +10,9 @@ public class CharacterControl : MonoBehaviour {
 	public float gravity = 20.0f;
 	public float speed = 0;
 	private Vector3 moveDirection = Vector3.zero;
-	public GameObject pauseMenu;
+	//public GameObject pauseMenu;
 	private bool menuOpen;
+	public GameObject map;
 
 
 	// Use this for initialization
@@ -21,7 +22,7 @@ public class CharacterControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		PauseGame ();
+		//PauseGame ();
 		CharacterController controller = GetComponent<CharacterController> ();
 		moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
 		moveDirection = transform.TransformDirection (moveDirection);
@@ -51,9 +52,10 @@ public class CharacterControl : MonoBehaviour {
 		moveDirection.y -= (gravity * (Time.deltaTime));
 		//print(moveDirection.y);
 		controller.Move (moveDirection * Time.deltaTime);
+		map.SetActive (false);
 	}
 
-	void PauseGame()
+	/*void PauseGame()
 	{
 		if (Input.GetKeyDown (KeyCode.Tab) && menuOpen == false) {
 			print ("Menu Activated");
@@ -66,5 +68,5 @@ public class CharacterControl : MonoBehaviour {
 			menuOpen = false;
 
 		}
-	}
+	}*/
 }
