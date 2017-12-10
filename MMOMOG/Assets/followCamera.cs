90a53d2d25edc6b0fdd723db6bf4c3a258fa5744
Tigerish <tigerishous@gmail.com>
2017-12-09 21:32:07 -0600

    Test Camera Look
    Very buggy

---
+++ MMOMOG/Assets/followCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.LookAt (target);
+		transform.Translate (Vector3.up * 1, Space.World);
+		transform.Translate (Vector3.back * 1, Space.World);
+		if (target)
+		{
+			transform.position = Vector3.Lerp(transform.position, target.position, 0.4f);
+	}
+}
+}
