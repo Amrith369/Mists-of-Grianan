e93a9211733d340eb123d69c1cfdb2c9c47fa07d
Tigerish <tigerishous@gmail.com>
2017-12-11 03:39:17 -0600

    Camera (Needs fiddling)
    You can replace it with old camera if too much work.

--- MMOMOG/Assets/followCamera.cs
+++ MMOMOG/Assets/followCamera.cs
@@ -1,58 +1,125 @@
-﻿﻿using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class followCamera : MonoBehaviour
-{
-	public GameObject player;
-	public float distance;
-	public float speed;
-	public Vector3 velocity;
+public class followCamera : MonoBehaviour {
 
-	private bool z;
+	public Transform target;
 
-	void Update ()
+	[System.Serializable]
+	public class PositionSettings
 	{
-		float dist = Vector3.Distance (player.transform.position, transform.position);
+		public Vector3 targetPosOffset = new Vector3(0,-3.4f,0);
+		public float lookSmooth = 100f;
+		public float distanceFromTarget = -8;
+		public float zoomSmooth = 10;
+		public float maxZoom = -2;
+		public float minZoom = -15;
+	}
 
-		Vector3 behind = player.transform.position - new Vector3 (player.transform.forward.x * distance, player.transform.forward.y - 1.0f, player.transform.forward.z * distance);
+	[System.Serializable]
+	public class OrbitSettings
+	{
+		public float xRotation = -20;
+		public float yRotation = -180;
+		public float maxXRotation = 25;
+		public float minXRotation = -85;
+		public float vOrbitSmooth = 150;
+		public float hOrbitSmooth = 150;
+	}
 
-		if(!z && Input.GetButton("Horizontal"))
+	[System.Serializable]
+	public class InputSettings
+	{
+		public string ORBIT_HORIZONTAL_SNAP = "OrbitHorizontalSnap";
+		public string ORBIT_HORIZONTAL = "OrbitHorizontal";
+		public string ORBIT_VERTICAL = "OrbitVertical";
+		public string ZOOM = "Mouse ScrollWheel";
+	}
+
+	public PositionSettings position = new PositionSettings();
+	public OrbitSettings orbit = new OrbitSettings();
+	public InputSettings input = new InputSettings();
+
+	Vector3 targetPos = Vector3.zero;
+	Vector3 destination = Vector3.zero;
+	CharacterController charController;
+	float vOrbitInput, hOrbitInput, zoomInput, hOrbitSnapInput;
+
+	void Start()
+	{
+		SetCameraTarget(target);
+	}
+	void SetCameraTarget(Transform t)
+	{
+		target = t;
+		if (target != null) {
+			if (target.GetComponent<CharacterController> ()) {
+				charController = target.GetComponent<CharacterController> ();
+			} else
+				Debug.LogError ("The camera's target needs a character controller.");
+		} else
+			Debug.LogError ("Your camera needs a target.");
+	}
+	void GetInput()
+	{
+		vOrbitInput = Input.GetAxisRaw (input.ORBIT_VERTICAL);
+		hOrbitInput = Input.GetAxisRaw (input.ORBIT_HORIZONTAL);
+		hOrbitSnapInput = Input.GetAxisRaw (input.ORBIT_HORIZONTAL_SNAP);
+		zoomInput = Input.GetAxisRaw (input.ZOOM);
+	}
+	void Update()
+	{
+		GetInput ();
+		OrbitTarget ();
+		ZoomInOnTarget ();
+	}
+	void LateUpdate()
+	{
+		MoveToTarget ();
+		LookAtTarget ();
+	}
+
+	void MoveToTarget()
+	{
+		targetPos = target.position + position.targetPosOffset;
+		destination = Quaternion.Euler (orbit.xRotation, orbit.yRotation, 0) * -Vector3.forward * position.distanceFromTarget;
+		destination += targetPos;
+		transform.position = destination;
+	}
+	void LookAtTarget()
+	{
+		Quaternion targetRotation = Quaternion.LookRotation (targetPos - transform.position);
+		transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, position.lookSmooth * Time.deltaTime);
+	}
+	void OrbitTarget()
+	{
+		if (hOrbitSnapInput > 0)
 		{
-			z = true;
+			orbit.yRotation = -180;
 		}
-		else if(z)
+		orbit.xRotation += -vOrbitInput * orbit.vOrbitSmooth * Time.deltaTime;
+		orbit.yRotation += -hOrbitInput * orbit.hOrbitSmooth * Time.deltaTime;
+		if (orbit.xRotation > orbit.maxXRotation)
 		{
-			transform.LookAt(player.transform);
-
-			transform.position = Vector3.SmoothDamp(transform.position, behind, ref velocity, Time.deltaTime);
-			print ("1");
-
-			if(transform.position == behind)
-			{
-				z = false;
-			}
+			orbit.xRotation = orbit.maxXRotation;
 		}
-		else
+		if (orbit.xRotation < orbit.minXRotation)
 		{
-			if (dist < distance)
-			{
-				transform.LookAt(player.transform);
-			}
-			else if (dist > distance * 2)
-			{
-				transform.LookAt(player.transform);
-				transform.position = Vector3.SmoothDamp(transform.position, behind, ref velocity, Time.deltaTime * 2);
-
-				//transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y + 1.0f, player.transform.position.z - distance), speed * 30 * Time.deltaTime / 1000);
-				//print ("2");
-			}
-			else if (dist < distance * 2 && dist > distance)
-			{
-				transform.LookAt(player.transform);
-
-				transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y + 1.0f, player.transform.position.z - distance), speed * Time.deltaTime / 1000);
-				print ("3");
-			}
+			orbit.xRotation = orbit.minXRotation;
+		}
+	}
+	void ZoomInOnTarget()
+	{
+		position.distanceFromTarget += zoomInput * position.zoomSmooth * Time.deltaTime;
+		if (position.distanceFromTarget > position.maxZoom)
+		{
+			position.distanceFromTarget = position.maxZoom;
+		}
+		if (position.distanceFromTarget < position.minZoom)
+		{
+			position.distanceFromTarget = position.minZoom;
 		}
 	}
 }
+
+
